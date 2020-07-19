bd8ae4b3d584a80f1ad9bc466d263414bf9382c0
Phan Hieu <pthieu2812@gmail.com>
2020-07-19 21:23:40 +0700

    Add Splash Screen Add App Icon Add App Name

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar (
+        title: Text('Fast Kara'),
+      ),
+      body: Center(
+        child: Text('Welcome to Fast Kara',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0
+                ),
+        ),
+      ),
+    ); //Scaffold
+  }
+}
