51acaed84aaa6f7cdd8c060f6c79720d380f3082
pthieu2812 <57866291+pthieu2812@users.noreply.github.com>
2020-07-23 23:39:45 +0700

    Refactor splash screen (#5)
    * Temp
    
    * ReFactor Splash Screen
    Change App Icon

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,20 +1,39 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    Color appBarColor = const Color.fromRGBO(33, 38, 43, 1.0);
+    Color backgroundColor = const Color.fromRGBO(31, 31, 31, 1.0);
     return Scaffold(
+      backgroundColor: backgroundColor,
       appBar: AppBar (
         title: Text('FastKara'),
+        backgroundColor: appBarColor,
       ),
       body: Center(
-        child: Text('Welcome to Fast Kara',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20.0
-                ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new FlatButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              disabledColor: Colors.grey,
+              disabledTextColor: Colors.black,
+              padding: EdgeInsets.all(8.0),
+              splashColor: Colors.blueAccent,
+              onPressed: () {
+                /*...*/
+              },
+              child: Text(
+                "Flat Button",
+                style: TextStyle(fontSize: 20.0),
+              ),
+            )
+          ],
+          ),
         ),
-      ),
-    ); //Scaffold
+      );
   }
 }
