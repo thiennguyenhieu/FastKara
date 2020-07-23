8b98db7c07d806db42b021e5318fdb10aff7074b
Phan Hieu <pthieu2812@gmail.com>
2020-07-23 23:33:25 +0700

    ReFactor Splash Screen Change App Icon

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 import 'home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -9,6 +10,12 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  Color backgroundColor = const Color.fromRGBO(31, 31, 31, 1.0);
+  Color splashTextBaseColor = const Color.fromRGBO(255, 218, 159, 1.0);
+  Color splashTextHighlight = const Color.fromRGBO(108, 94, 76, 1.0);
+  Color textColor = const Color.fromRGBO(92, 92, 92, 1.0);
+
   @override
   void initState() {
     super.initState();
@@ -21,14 +28,59 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: backgroundColor,
+      body: Container(
+        child: new Stack(
           children: <Widget>[
-            Opacity(
-                opacity: 0.5,
-                child: Image.asset('assets/images/splash_background.jpg')
+            new Positioned(
+              child: new Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 80),
+                  child: Shimmer.fromColors(
+                      child: Text(
+                        'FASTKARA',
+                        style: TextStyle(
+                            fontSize: 60.0,
+                            fontFamily: 'Bebas'
+                        ),
+                      ),
+                      baseColor: splashTextHighlight,
+                      highlightColor: splashTextBaseColor
+                  ),
+                )
+              ),
+            ),
+            new Positioned(
+                child: new Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 100),
+                    child: Text(
+                      'from',
+                      style: TextStyle(
+                        color: textColor,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  )
+                )
+            ),
+            new Positioned(
+                child: new Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 80),
+                    child: Text(
+                      'FASTAPP STUDIO',
+                      style: TextStyle(
+                        color: splashTextBaseColor,
+                        fontFamily: 'Fialla',
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ),
+                )
             )
           ],
         ),
