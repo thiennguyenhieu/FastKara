43dda3b0862c080d7a64aa41c59d4e55ebb7cf7f
thiennguyenhieu <thien.nguyenhieu@gmail.com>
2020-07-28 01:35:21 +0700

    Change display of ListView on Home tab

--- lib/ui/home_tab_page.dart
+++ lib/ui/home_tab_page.dart
@@ -6,7 +6,8 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
-  final List<String> items = List<String>.generate(10000, (i) => "Title $i");
+  final List<String> items =
+      List<String>.generate(10000, (i) => "Em Không Sai Chúng Ta Sai");
 
   @override
   Widget build(BuildContext context) {
@@ -16,36 +17,43 @@ class _HomeTabState extends State<HomeTab> {
         title: const Text('Popular Songs',
             style: TextStyle(color: const Color.fromRGBO(255, 218, 159, 1.0))),
       ),
-      backgroundColor: Colors.black,
+      backgroundColor: Color.fromRGBO(31, 31, 31, 1.0),
       body: ListView.builder(
         itemCount: items.length = items.length,
         itemBuilder: (context, index) {
           return Card(
-              color: Colors.white,
+              elevation: 0,
+              color: Colors.transparent,
               child: ListTile(
-                leading: Icon(Icons.image),
-                title: Text('${items[index]}'),
-                subtitle: Text('${items[index]}'),
+                leading: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minWidth: 60,
+                      minHeight: 60,
+                      maxWidth: 60,
+                      maxHeight: 60,
+                    ),
+                    child: Image.network(
+                        'https://109cdf7de.vws.vegacdn.vn/v1/banner/528.jpg?t=1593536400')),
+                title: Text('${items[index]}',
+                    style: TextStyle(color: Colors.white)),
+                subtitle: Text('${items[index]}',
+                    style: TextStyle(color: Colors.white70)),
                 trailing: Wrap(
                   children: <Widget>[
                     IconButton(
-                      icon: Icon(Icons.favorite_border),
+                      icon: Icon(Icons.more_horiz),
+                      color: Colors.white70,
                       onPressed: () {
                         setState(() {
-                          // Todo: Update icon
-                        });
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.file_download),
-                      onPressed: () {
-                        setState(() {
-                          // Todo: Update icon
+                          // ToDo: Display menu popup
                         });
                       },
                     ),
                   ],
                 ),
+                onTap: () {
+                  // ToDo: Go to play_song_page
+                },
               ));
         },
       ),
