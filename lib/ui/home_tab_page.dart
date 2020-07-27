7cdca57a3866dd04acc4e607dc2feec3dd4f39c5
thiennguyenhieu <thien.nguyenhieu@gmail.com>
2020-07-27 23:20:45 +0700

    Add ListView implemetation on Home tab

--- lib/ui/home_tab_page.dart
+++ lib/ui/home_tab_page.dart
@@ -1,28 +1,53 @@
 import 'package:flutter/material.dart';
 
-class HomeTab extends StatelessWidget {
+class HomeTab extends StatefulWidget {
+  @override
+  _HomeTabState createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  final List<String> items = List<String>.generate(10000, (i) => "Title $i");
+
   @override
   Widget build(BuildContext context) {
-    Color backgroundColor = const Color.fromRGBO(31, 31, 31, 1.0);
     return Scaffold(
-      backgroundColor: backgroundColor,
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(
-                Icons.home,
-                size: 100.0,
-                color: Colors.white,
-              ),
-              Text(
-                "HomeScreen Tab",
-                style: TextStyle(color: Colors.white),
-              )
-            ],
-          ),
-        ),
+      appBar: new AppBar(
+        backgroundColor: Color.fromRGBO(31, 31, 31, 1.0),
+        title: const Text('Popular Songs',
+            style: TextStyle(color: const Color.fromRGBO(255, 218, 159, 1.0))),
+      ),
+      backgroundColor: Colors.black,
+      body: ListView.builder(
+        itemCount: items.length = items.length,
+        itemBuilder: (context, index) {
+          return Card(
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(Icons.image),
+                title: Text('${items[index]}'),
+                subtitle: Text('${items[index]}'),
+                trailing: Wrap(
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.favorite_border),
+                      onPressed: () {
+                        setState(() {
+                          // Todo: Update icon
+                        });
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.file_download),
+                      onPressed: () {
+                        setState(() {
+                          // Todo: Update icon
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ));
+        },
       ),
     );
   }
