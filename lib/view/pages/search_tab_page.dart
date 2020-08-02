13eb5d328150cbd825744d36b2606190f80051f5
minwon310197 <48650525+minwon310197@users.noreply.github.com>
2020-08-02 23:45:16 +0700

    Fix issue: When routed to play_song_page, BottomNavigationBar still display. (#14)

--- lib/view/pages/search_tab_page.dart
+++ lib/view/pages/search_tab_page.dart
@@ -3,13 +3,12 @@ import 'package:fast_kara/model/song_model.dart';
 import 'package:fast_kara/static/const_color.dart';
 import 'package:fast_kara/view/pages/play_song_page.dart';
 import 'package:fast_kara/view/widgets/custom_route.dart';
+import 'package:fast_kara/view/widgets/custom_search_bar.dart';
 import 'package:fast_kara/view/widgets/list_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:loading/indicator/ball_pulse_indicator.dart';
 import 'package:loading/loading.dart';
-
-import '../../api/search_bar_api.dart';
 
 class SearchTab extends StatefulWidget {
   @override
@@ -104,7 +103,8 @@ class SearchWidgetState extends State<SearchTab>
         ),
         child: Scaffold(
             appBar: CupertinoNavigationBar(
-              middle: SearchBarAPI(
+              backgroundColor: Colors.black,
+              middle: CustomSearchBar(
                 controller: _searchTextController,
                 focusNode: _searchFocusNode,
                 animation: _animation,
@@ -115,31 +115,28 @@ class SearchWidgetState extends State<SearchTab>
             backgroundColor: Colors.black,
             body: Container(
                 child: FutureBuilder(
-                        future: _songBook,
-                        builder:
-                            (BuildContext context, AsyncSnapshot listSong) {
-                          if (listSong.data == null) {
-                            return Container(
-                              child: Center(
-                                child: Loading(
-                                    indicator: BallPulseIndicator(),
-                                    size: 50.0),
-                              ),
-                            );
-                          } else {
-                            return ListView.builder(
-                                itemCount: listSong.data.length,
-                                itemBuilder: (BuildContext context, int index) {
-                                  return ListItem(
-                                      imageUrl: listSong.data[index].imgUrl,
-                                      title: listSong.data[index].title,
-                                      subtitle: listSong.data[index].singer,
-                                      onMoreBtnPressed: _onMoreBtnPressed);
-                                });
-                          }
-                        },
-                      )
-)));
+              future: _songBook,
+              builder: (BuildContext context, AsyncSnapshot listSong) {
+                if (listSong.data == null) {
+                  return Container(
+                    child: Center(
+                      child:
+                          Loading(indicator: BallPulseIndicator(), size: 50.0),
+                    ),
+                  );
+                } else {
+                  return ListView.builder(
+                      itemCount: listSong.data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListItem(
+                            imageUrl: listSong.data[index].imgUrl,
+                            title: listSong.data[index].title,
+                            subtitle: listSong.data[index].singer,
+                            onMoreBtnPressed: _onMoreBtnPressed);
+                      });
+                }
+              },
+            ))));
   }
 
   void _onMoreBtnPressed() {}
