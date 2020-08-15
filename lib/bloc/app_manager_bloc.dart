a4a37661245ad7cae3a7ba1ff5676962d0312d93
minwon310197 <minhquannguyen97@gmail.com>
2020-08-16 00:30:31 +0700

    [SearchTabPage] Apply bloc part (I) and Redesign layout.

--- lib/bloc/app_manager_bloc.dart
+++ lib/bloc/app_manager_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:fast_kara/bloc/play_song_bloc.dart';
+import 'package:fast_kara/bloc/search_manager_bloc.dart';
 import 'package:fast_kara/bloc/song_book_bloc.dart';
 import 'package:fast_kara/bloc/sign_in_bloc.dart';
 
@@ -6,14 +7,17 @@ class AppManagerBloc {
   SongBookBloc _songBookBloc;
   SignInBloc _signInBloc;
   PlaySongBloc _playSongBloc;
+  SearchManagerBloc _searchManagerBloc;
 
   AppManagerBloc() {
     _songBookBloc = SongBookBloc();
     _signInBloc = SignInBloc();
     _playSongBloc = PlaySongBloc();
+    _searchManagerBloc = SearchManagerBloc();
   }
 
   SongBookBloc get songBookBloc => _songBookBloc;
   SignInBloc get signInBloc => _signInBloc;
   PlaySongBloc get playSongBloc => _playSongBloc;
+  SearchManagerBloc get searchManagerBloc => _searchManagerBloc;
 }
--- lib/view/pages/search_tab_page.dart
+++ lib/view/pages/search_tab_page.dart
@@ -1,38 +1,36 @@
-import 'package:fast_kara/static/const_color.dart';
+import 'package:fast_kara/bloc/bloc_provider.dart';
+import 'package:fast_kara/model/song_model.dart';
+import 'package:fast_kara/view/pages/play_song_page.dart';
 import 'package:fast_kara/view/widgets/custom_search_bar.dart';
-import 'package:flutter/material.dart';
+import 'package:fast_kara/view/widgets/songbook_list_item.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fast_kara/static/const_color.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:fast_kara/bloc/search_manager_bloc.dart';
 
 class SearchTab extends StatefulWidget {
+  final SearchManagerBloc bloc;
+
+  const SearchTab(this.bloc);
+
   @override
-  State<StatefulWidget> createState() {
-    return SearchWidgetState();
-  }
+  State<StatefulWidget> createState() => _SearchWidgetState(this.bloc);
 }
 
-class SearchWidgetState extends State<SearchTab>
+class _SearchWidgetState extends State<SearchTab>
     with SingleTickerProviderStateMixin {
   TextEditingController _searchTextController = TextEditingController();
-  FocusNode _searchFocusNode = FocusNode();
   Animation _animation;
   AnimationController _animationController;
   String _searchTextInProgress;
 
-  var _defaultrecent = [
-    'Sau tất cả',
-    'Ghen',
-    'Niềm tin chiến thắng',
-    'Gọi giấc mơ về',
-    'Màu nước mắt'
-  ];
-
-  var isItemSelected = true;
-  var itemSelectedText = '';
+  final SearchManagerBloc bloc;
+  _SearchWidgetState(this.bloc);
 
   @override
   initState() {
     super.initState();
-
     _animationController = new AnimationController(
       duration: new Duration(milliseconds: 200),
       vsync: this,
@@ -42,7 +40,8 @@ class SearchWidgetState extends State<SearchTab>
       curve: Curves.easeInOut,
       reverseCurve: Curves.easeInOut,
     );
-    _searchFocusNode.addListener(() {
+
+    bloc.getFocusNode().addListener(() {
       if (!_animationController.isAnimating) {
         _animationController.forward();
       }
@@ -71,15 +70,9 @@ class SearchWidgetState extends State<SearchTab>
 
   _cancelSearch() {
     _searchTextController.clear();
-    _searchFocusNode.unfocus();
+    bloc.getFocusNode().unfocus();
     _animationController.reverse();
-    this.setState(() {
-      _searchTextInProgress = null;
-    });
-  }
 
-  _clearSearch() {
-    _searchTextController.clear();
     this.setState(() {
       _searchTextInProgress = null;
     });
@@ -93,104 +86,155 @@ class SearchWidgetState extends State<SearchTab>
 
   @override
   Widget build(BuildContext context) {
-    return (isItemSelected)
-        ? CupertinoPageScaffold(
-            child: NestedScrollView(
-                headerSliverBuilder:
-                    (BuildContext context, bool innerBoxIsScrolled) {
-                  return <Widget>[
-                    CupertinoSliverNavigationBar(
-                      heroTag: 'searchtabpage',
-                      transitionBetweenRoutes: false,
-                      largeTitle: Text(
-                        'Search',
-                        style: TextStyle(color: AppColors.colorAppText),
-                      ),
-                      backgroundColor: Colors.black,
-                    ),
-                  ];
-                },
-                body: Scaffold(
-                  appBar: CupertinoNavigationBar(
-                    heroTag: 'searchtabpage',
-                    transitionBetweenRoutes: false,
-                    padding: EdgeInsetsDirectional.only(bottom: 10),
-                    backgroundColor: Colors.black,
-                    middle: CustomSearchBar(
-                      controller: _searchTextController,
-                      focusNode: _searchFocusNode,
-                      animation: _animation,
-                      onCancel: _cancelSearch,
-                      onClear: _clearSearch,
-                    ),
-                  ),
-                  backgroundColor: Colors.black,
-                  body: Scaffold(
-                    backgroundColor: Colors.black,
-                    appBar: CupertinoNavigationBar(
-                        heroTag: 'searchtabpage',
-                        transitionBetweenRoutes: false,
-                        backgroundColor: Colors.black,
-                        leading: Text('Recent',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 25.0,
-                                fontWeight: FontWeight.bold))),
-                    body: ListView.builder(
-                      itemCount: _defaultrecent.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: new Text(
-                            _defaultrecent[index],
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          onTap: () {
-                            onTapItem(_defaultrecent[index]);
-                          },
-                        );
-                      },
-                    ),
-                  ),
-                )))
-        : CupertinoPageScaffold(
-            navigationBar: CupertinoNavigationBar(
-              heroTag: 'searchtabpage',
-              transitionBetweenRoutes: false,
-              backgroundColor: Colors.black,
-              leading: CupertinoNavigationBarBackButton(
-                  color: AppColors.colorAppText,
-                  onPressed: () => onTapItem(itemSelectedText)),
-              middle: Text(
-                """Search for "$itemSelectedText" """,
-                style: TextStyle(color: AppColors.colorAppText, fontSize: 20.0),
-              ),
-            ),
-            child: Scaffold(
-              appBar: null,
-              backgroundColor: Colors.black,
-              body: ListView.builder(
-                itemCount: _defaultrecent.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: new Text(
-                      '$itemSelectedText $index',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  );
-                },
-              ),
-            ),
-          );
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          heroTag: 'searchtabpage',
+          transitionBetweenRoutes: false,
+          backgroundColor: Colors.black,
+          middle: CustomSearchBar(
+            controller: _searchTextController,
+            focusNode: bloc.getFocusNode(),
+            animation: _animation,
+            onCancel: _cancelSearch,
+            onClear: _cancelSearch,
+          ),
+        ),
+        child: _SearchPageBody());
+  }
+}
+
+class _SearchPageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: CupertinoNavigationBar(
+          padding: EdgeInsetsDirectional.only(top: 8.0, start: 8.0),
+          heroTag: 'searchtabpage',
+          transitionBetweenRoutes: false,
+          backgroundColor: Colors.black,
+          leading: Text('Search',
+              style: TextStyle(
+                color: AppColors.colorAppText,
+                fontSize: 20.0,
+              )),
+        ),
+        backgroundColor: AppColors.colorAppBackground,
+        body: _SongBookList());
+  }
+}
+
+class _SongBookList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = BlocProvider.of(context).songBookBloc;
+
+    return Container(
+      child: StreamBuilder<List<SongModel>>(
+        initialData: [],
+        stream: bloc.updateSongBook,
+        builder: (context, snapshot) {
+          if (snapshot.data.length > 0) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                SongModel song = snapshot.data[index];
+                return SongBookListItem(
+                  imageUrl: song.imgUrl,
+                  title: song.title,
+                  subtitle: song.singer,
+                  onItemTab: () {
+                    _navigateToSubPage(context, song);
+                  },
+                  onMoreBtnPressed: () {
+                    _onMoreBtnPressed(context, song);
+                  },
+                );
+              },
+            );
+          } else {
+            return Center(
+                child: CircularProgressIndicator(
+              strokeWidth: 5,
+              valueColor:
+                  new AlwaysStoppedAnimation<Color>(AppColors.colorAppText),
+            ));
+          }
+        },
+      ),
+    );
   }
 
-  void onTapItem(String value) {
-    this.setState(() {
-      if (!isItemSelected) {
-        itemSelectedText = '';
-      } else {
-        itemSelectedText = value;
-      }
-      isItemSelected = !isItemSelected;
-    });
+  Future _navigateToSubPage(context, SongModel song) async {
+    Navigator.of(context, rootNavigator: true)
+        .push(CupertinoPageRoute(builder: (context) => PlaySongPage(song)));
+  }
+
+  void _onMoreBtnPressed(context, SongModel song) {
+    showBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            height: 200,
+            color: Colors.grey[900],
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  leading: (Image.network(song.imgUrl)),
+                  title: Text(
+                    song.title,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  subtitle: Text(
+                    song.singer,
+                    style: TextStyle(color: Colors.white60),
+                  ),
+                ),
+                Divider(
+                  color: Colors.white,
+                  endIndent: 40,
+                  indent: 40,
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    "Add To Favorite",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: _onPressFavorite,
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.file_download,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    "Download",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: _onPressDownload,
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  void _onPressFavorite() {
+    Fluttertoast.showToast(
+        msg: "Added to Favorite",
+        backgroundColor: Colors.white,
+        textColor: Colors.black,
+        toastLength: Toast.LENGTH_SHORT);
+  }
+
+  void _onPressDownload() {
+    Fluttertoast.showToast(
+        msg: "Downloading...",
+        backgroundColor: Colors.white,
+        textColor: Colors.black,
+        toastLength: Toast.LENGTH_SHORT);
   }
 }
