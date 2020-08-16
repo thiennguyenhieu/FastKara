6ed523566e16dc3579f529df6f450ca86028fd02
minwon310197 <48650525+minwon310197@users.noreply.github.com>
2020-08-16 08:43:46 +0700

    [SearchTabPage] Apply bloc part (I) and Redesign layout. (#19)

--- lib/view/screens/main_screen.dart
+++ lib/view/screens/main_screen.dart
@@ -1,3 +1,5 @@
+import 'package:fast_kara/bloc/bloc_provider.dart';
+import 'package:fast_kara/bloc/search_manager_bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,22 +8,35 @@ import 'package:fast_kara/view/pages/home_tab_page.dart';
 import 'package:fast_kara/view/pages/user_tab_page.dart';
 import 'package:fast_kara/view/pages/search_tab_page.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   @override
-  _MainScreenState createState() => _MainScreenState();
+  Widget build(BuildContext context) {
+    final searchTabBloc = BlocProvider.of(context).searchManagerBloc;
+    return _MainScreen(searchTabBloc);
+  }
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreen extends StatefulWidget {
+  final SearchManagerBloc bloc;
+
+  const _MainScreen(this.bloc);
+  @override
+  _MainScreenState createState() => _MainScreenState(bloc);
+}
+
+class _MainScreenState extends State<_MainScreen> {
   List<Widget> _pages;
   Widget _pageHome;
   Widget _pageSearch;
   Widget _pageUserAccount;
+  final SearchManagerBloc searchTabBloc;
+  _MainScreenState(this.searchTabBloc);
 
   @override
   void initState() {
     super.initState();
     _pageHome = HomeTab();
-    _pageSearch = SearchTab();
+    _pageSearch = SearchTab(searchTabBloc);
     _pageUserAccount = UserAccountTab();
     _pages = [_pageHome, _pageSearch, _pageUserAccount];
   }
@@ -44,6 +59,11 @@ class _MainScreenState extends State<MainScreen> {
               BottomNavigationBarItem(
                   icon: Icon(Icons.account_circle), title: Container()),
             ],
+            onTap: (index) {
+              if (index == 1) {
+                searchTabBloc.requestFocus();
+              }
+            },
           ),
           tabBuilder: (context, index) {
             return CupertinoTabView(builder: (context) {
